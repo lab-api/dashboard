e43029fbfb59bb5bcd9b894e51f311a90f40dff9
Robert Fasano <robert.j.fasano@colorado.edu>
2020-02-04 16:41:24 -0700

    Add new instruments and bounds

--- dashboard/test/debug.py
+++ dashboard/test/debug.py
@@ -7,8 +7,8 @@ class TAPro(Instrument):
     def __init__(self, name='TA Pro'):
         super().__init__(name=name)
         self.frequency = Knob('frequency', 394798.25)
-        self.current = Knob('current', 126)
-        self.voltage = Knob('voltage', 74.6)
+        self.current = Knob('current', 126, bounds=(0, 130))
+        self.voltage = Knob('voltage', 74.6, bounds=(0, 140))
         self.emission = Switch('emission', False)
         self.temperature = Knob('temperature', 30)
         self.wavemeter_reading = Measurement('wavemeter', self.read_wavemeter)
@@ -19,12 +19,29 @@ class TAPro(Instrument):
 class Coils(Instrument):
     def __init__(self, name='Coils'):
         super().__init__(name=name)
-        self.gradient = Knob('gradient', 50.2)
-        self.offset = Knob('offset', 3)
+        self.gradient = Knob('gradient', 50.2, bounds=(0, 80))
+        self.offset = Knob('offset', 3, bounds=(-10, 10))
         self.powered = Switch('powered', False)
 
-inst = TAPro(name='laser')
+class IntensityServo(Instrument):
+    def __init__(self, name='IntensityServo'):
+        super().__init__(name=name)
+        self.setpoint = Knob('setpoint', 4.1, bounds=(0, 5))
+        self.lock = Switch('lock', False)
+
+class PMT(Instrument):
+    def __init__(self, name='PMT'):
+        super().__init__(name=name)
+        self.voltage = Knob('voltage', 0.7, bounds=(0, 1))
+        self.fluorescence = Measurement('fluorescence', self.measure_fluorescence)
+
+    def measure_fluorescence(self):
+        return 10**self.voltage
+
+inst = TAPro(name='Laser')
 inst2 = Coils()
+inst3 = IntensityServo()
+pmt = PMT()
 
 api = API(globals(), debug=True, port=8000)
 api.serve()
